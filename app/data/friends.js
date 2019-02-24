757bd11d06d18ac471949f1a2125d7f994795dc4
Mvcampbell3 <mvcampbell3@gmail.com>
2019-02-24 02:11:05 -0500

    hope I didn't break everything with submole somthing

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+const friends = [
+    {
+        name: "Saturn V",
+        photo: "Havenotdoneyet.jpg",
+        scores: [
+            5,
+            3,
+            2,
+            1,
+            3,
+            4,
+            5,
+            4,
+            2,
+            1
+        ]
+    }
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
-
+let friends = require("./app/data/friends.js");
 const app = express();
+require("./routing/htmlRoutes")(app);
+
+
 
 // Init Middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send("All is well on the server");
-})
+
+
 
 app.listen(PORT, ()=> {
     console.log("Server is running on http://localhost:5000");
